decd761d0120c9b75e39596b9748ee2c695c8e5c
madhundle <madeline.hundley@gmail.com>
2024-04-15 09:00:14 -0500

    feat(section2): :sparkles: section2-17
    Course material complete through section 2-17.

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {UntypedFormBuilder, Validators} from '@angular/forms';
+import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 
 
 @Component({
@@ -21,6 +22,15 @@ export class CreateCourseStep1Component {
     downloadsAllowed: [false, Validators.requiredTrue],
     longDescription: ['', [Validators.required, Validators.minLength(3)]]
   });
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    // For the first day of every month, apply the highlight-date class
+    const date = cellDate.getDate();
+    if (view == 'month') {
+      return (date == 1) ? 'highlight-date' : "";
+    }
+    return "";
+  }
 
   constructor(private fb: UntypedFormBuilder) {
 
