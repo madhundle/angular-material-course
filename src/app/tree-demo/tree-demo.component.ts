fd70d8595c73f0223ed41cefd0a644b1a6e6c0ee
madhundle <madeline.hundley@gmail.com>
2024-04-17 10:44:41 -0500

    feat(section6) :sparkles: section6-3
    Completed section6: Trees

--- src/app/tree-demo/tree-demo.component.ts
+++ src/app/tree-demo/tree-demo.component.ts
@@ -7,6 +7,12 @@ interface CourseNode {
   children?: CourseNode[];
 }
 
+interface CourseFlatNode {
+  name: string;
+  expandable: boolean;
+  level: number;
+}
+
 const TREE_DATA: CourseNode[] = [
   {
     name: 'Angular For Beginners',
@@ -58,12 +64,48 @@ const TREE_DATA: CourseNode[] = [
 })
 export class TreeDemoComponent implements OnInit {
 
+  /* Nested */
+
+  nestedDataSource = new MatTreeNestedDataSource<CourseNode>();
+  // NestedTreeControl takes a function which can extract a node's children
+  nestedTreeControl = new NestedTreeControl<CourseNode>(node => node.children);
+
+  /* Flat */
+
+  // FlatTreeControl takes functions to get the level and whether it's expandable
+  flatTreeControl = new FlatTreeControl<CourseFlatNode>(
+    node => node.level,
+    node => node.expandable
+  );
+  // To craft our flatDataSource, we need to flatten our data
+  // Provide a transform function for CourseNode -> CourseFlatNode
+  // Also provide level, expandable, and children
+  treeFlattener = new MatTreeFlattener(
+    (node: CourseNode, level: number): CourseFlatNode => {
+      return {
+        name: node.name,
+        expandable: node.children?.length > 0,
+        level
+      }
+    },
+    node => node.level,
+    node => node.expandable,
+    node => node.children
+  );
+  flatDataSource = new MatTreeFlatDataSource(this.flatTreeControl, this.treeFlattener);
 
   ngOnInit() {
-
-
+    this.nestedDataSource.data = TREE_DATA;
+    this.flatDataSource.data = TREE_DATA;
   }
 
+  hasNestedChild(index: number, node: CourseNode) {
+    return node?.children?.length > 0;
+  }
+
+  hasFlatChild(index: number, node: CourseFlatNode) {
+    return node.expandable;
+  }
 }
 
 
